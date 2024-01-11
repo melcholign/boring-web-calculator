4dfa4d6909bdd5a32c790e1c9765b04d781ca775
melcholign <115886450+melcholign@users.noreply.github.com>
2024-01-12 00:56:13 +0600

    Add calculation logic and display functions
    The operations object aid in easily identifying and retrieving the
    operator associated function.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let x, y, op;
+x = y = op = '';
+
+const operators = {
+    '+': add,
+    '-': subtract,
+    '×': multiply,
+    '÷': divide,
+}
+
+const currentDisplay = document.querySelector('#display #current');
 
 function add(a, b) {
     return a + b;
@@ -19,3 +29,43 @@ function divide(a, b) {
 function operate(a, op, b) {
     return op(a, b);
 }
+
+function evaluate() {
+    if (x && op && y) {
+        x = operate(+x, operators[op], +y);
+        op = '';
+        y = '';
+    }
+}
+
+function updateDisplay() {
+    currentDisplay.value = `${x ? x + ' ' : ''}${x && op ? op + ' ' : ''}${y ? y + ' ' : ''}`;
+}
+
+document.querySelector('#controls').addEventListener('click', event => {
+    if (event.target.tagName !== 'BUTTON') return;
+
+    const btnTxt = event.target.textContent;
+
+    if ('0' <= btnTxt && btnTxt <= '9') {
+
+        if (op) {
+            y += btnTxt;
+        } else {
+            x += btnTxt;
+        }
+
+    } else if (btnTxt in operators) {
+
+        if (y) {
+            evaluate();
+        }
+
+        op = btnTxt;
+
+    } else if (btnTxt === '=') {
+        evaluate();
+    }
+
+    updateDisplay();
+});
